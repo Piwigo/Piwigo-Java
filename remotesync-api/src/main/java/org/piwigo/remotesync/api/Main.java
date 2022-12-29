9655e7e273ef04e5d8def895bdbbb09ed42a3228
mingan <11653241+mingan666@users.noreply.github.com>
2022-12-29 15:58:52 +0100

    Fix cmd line version

--- remotesync-api/src/main/java/org/piwigo/remotesync/api/Main.java
+++ remotesync-api/src/main/java/org/piwigo/remotesync/api/Main.java
@@ -4,13 +4,15 @@
  * are made available under the terms of the GNU Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * 
+ *
  * Contributors:
  *     Matthieu Helleboid - initial API and implementation
  ******************************************************************************/
 package org.piwigo.remotesync.api;
 
 import org.piwigo.remotesync.api.AbstractMain;
+import org.piwigo.remotesync.api.sync.LoginJob;
+import org.piwigo.remotesync.api.sync.SyncJob;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,9 +22,12 @@ public class Main extends AbstractMain {
 	public static void main(String[] args) {
 		new Main().run(args);
 	}
-	
+
 	protected void start() {
 		logger.debug("will start batch Remotesync");
+		LoginJob preJob = new LoginJob();
+		preJob.execute();
+		new SyncJob(preJob).execute();
 	}
 
 //	// TODO implement dry run
